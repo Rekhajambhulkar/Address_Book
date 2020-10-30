a3a2cb5bc7670dc7d794978c3ecf0aa44e766b8d
Rekhajambhulkar <jambhulkarrekha@gmail.com>
2020-10-30 10:45:13 +0530

    Add Multiple addressbook to the system

--- MainClass.java
+++ MainClass.java
@@ -6,12 +6,23 @@ public class MainClass {
 
 	public static void main(String[] args) {
 		System.out.println("Welcome in AddressBook Program");
-		HashMap<String, HashMap> list = new HashMap<String, HashMap>();
-		AddressBook3 book = new AddressBook3();
+		HashMap<String, AddressBook> list = new HashMap<String, AddressBook>();
+
+		AddressBook familyAddressBook = new AddressBook();
+		AddressBook friendsAddressBook = new AddressBook();
+		list.put("Family", familyAddressBook);
+		list.put("Friends", friendsAddressBook);
 
 		while (true) {
-			book.getUserChoice();
+			System.out.println("1: Family \n" + "2: Friend \n" + "3: Terminate the program");
+			int selectedOption = scanner.nextInt();
+			if (selectedOption == 1) {
+				familyAddressBook.getUserChoice();
+			} else if (selectedOption == 2) {
+				friendsAddressBook.getUserChoice();
+			} else if (selectedOption == 3) {
+				break;
+			}
 		}
 	}
 }
-
